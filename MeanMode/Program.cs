2df0e453e5abef1733bb4e7e5e388bb1006bff90
Kenan Casey <kenancasey@gmail.com>
2019-10-18 10:14:07 -0500

    initial commit

---
+++ MeanMode/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MeanMode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+
+        /**
+         * <summary> Determines if the mean and mode are the same value. </summary>
+         * <returns>true if the mean = the mode
+         * otherwise, false.</returns>
+         * <remarks> If there is more than one mode, then return false. </remarks>
+         */
+        public static bool MeanMode(int[] array)
+        {
+            return true;
+        }
+    }
+}
